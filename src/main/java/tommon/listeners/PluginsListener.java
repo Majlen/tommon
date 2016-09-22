2ffe7c12dfa806bf375c9393e7475c6340677af5
Milan Ševčík <majlen@civ.zcu.cz>
2016-09-22 17:46:55 +0200

    Using improved PluginsManager from tommon-lib.

--- src/main/java/tommon/listeners/PluginsListener.java
+++ src/main/java/tommon/listeners/PluginsListener.java
@@ -1,11 +1,7 @@
 package tommon.listeners;
 
-import tommon.annotations.JMXObject;
-import tommon.annotations.KeyValueObject;
-import tommon.managers.JMXManager;
-import tommon.managers.KeyValueManager;
+import tommon.managers.PluginsManager;
 import tommon.plugins.PluginConfig;
-import tommon.plugins.timers.Timers;
 import tommon.servlets.PluginAPIServlet;
 import tommon.servlets.PluginMainServlet;
 
@@ -13,33 +9,28 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration;
-import java.io.File;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Properties;
-import java.util.Arrays;
 
 public final class PluginsListener implements ServletContextListener {
     private ServletContext context = null;
 
-    ArrayList<Timers> timers = new ArrayList<Timers>();
     ArrayList<String> names = new ArrayList<String>();
+    //TODO: This should replace the 2 lists above
+    PluginConfig[] plugins = null;
 
     @Override
     public void contextDestroyed(ServletContextEvent event) {
-        for (Timers timer: timers) {
-            timer.cancel();
+        for (PluginConfig plugin: plugins) {
+            plugin.getTimer().cancel();
 
             // Lets the timers cancel hopefully. Link below hasn't provided better solution.
             // https://mail-archives.apache.org/mod_mbox/tomcat-users/201107.mbox/%3C4E2CB8BC.6050202@tmbsw.com%3E
             Thread.yield();
         }
 
-        timers.clear();
-
+        plugins = null;
         context = null;
     }
 
@@ -54,45 +45,9 @@ public final class PluginsListener implements ServletContextListener {
             pluginsDir = pluginsDir.replace("${catalina.base}", System.getProperty("catalina.base"));
 	        System.out.println(pluginsDir);
 
-            File dir = new File(pluginsDir);
-            File[] filesList = dir.listFiles();
-            for (File file : filesList) {
-                if (file.isFile() && file.getName().endsWith(".jar")) {
-                    ClassLoader cl = URLClassLoader.newInstance(new URL[]{file.toURI().toURL()}, context.getClassLoader());
-
-                    Properties pluginProperties = new Properties();
-                    pluginProperties.load(cl.getResourceAsStream("META-INF/plugins.properties"));
-                    String[] plugins = pluginProperties.getProperty("plugins").split(",");
-		            System.out.println(Arrays.toString(plugins));
-
-                    for (String plugin: plugins) {
-                        try {
-                            Class clazz = Class.forName(plugin, true, cl);
-
-                            Annotation[] anns = clazz.getAnnotations();
-			                System.out.println(Arrays.toString(anns));
-                            for (Annotation ann : anns) {
-                                PluginConfig config = null;
-                                if (ann.annotationType().equals(JMXObject.class)) {
-                                    config = JMXManager.loadPlugin(plugin, clazz, context);
-                                    timers.add(config.getTimer());
-                                } else if (ann.annotationType().equals(KeyValueObject.class)) {
-                                    config = KeyValueManager.loadPlugin(plugin, clazz, context);
-                                    timers.add(config.getTimer());
-                                } else {
-                                    System.out.println("Unknown plugin annotation "+ann.toString());
-                                }
-
-                                if (config != null) {
-                                    registerServlet(config);
-                                }
-                            }
-                        } catch (Exception e) {
-                            java.lang.System.out.println("Unable to load plugin "+plugin);
-                            System.out.println(e.getMessage());
-                        }
-                    }
-                }
+            plugins = PluginsManager.getPlugins(context.getClassLoader(), pluginsDir);
+            for (PluginConfig plugin: plugins) {
+                registerServlet(plugin);
             }
         } catch (IOException e) {
             System.out.println("ERROR: tommon.properties is non-existent or cannot be accessed");
