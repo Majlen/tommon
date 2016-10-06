f0906bd0695856bbc4a26404e7fec177df4b5187
Milan Ševčík <majlen@civ.zcu.cz>
2016-10-06 14:04:13 +0200

    Support getting oldest date from DB

--- src/main/java/tommon/servlets/PluginAPIServlet.java
+++ src/main/java/tommon/servlets/PluginAPIServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 /**
  * Created by majlen on 20.7.16.
@@ -15,19 +16,29 @@ public class PluginAPIServlet extends PluginServlet {
     private static final long serialVersionUID = 1L;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        int from = Integer.parseInt(request.getParameter("from"));
-        int to = Integer.parseInt(request.getParameter("to"));
-
-        response.setContentType("text/csv");
+        DBManager db = (DBManager) getServletContext().getAttribute("db");
+        String first = request.getParameter("first");
         PrintWriter out = response.getWriter();
 
-        DBManager db = (DBManager) getServletContext().getAttribute("db");
-        try {
-            db.printTableCSV(config.getTable(), config.getFields(), from, to, out);
-        } catch (Exception e) {
-            System.out.println("ERROR: Could not acces data in DB");
-            System.out.println(e.getMessage());
-            return;
+        if (first != null) {
+            try {
+                response.getWriter().print(db.getMinimumDate(config.getTable()));
+            } catch (SQLException e) {
+                response.getWriter().print(System.currentTimeMillis()/1000);
+            }
+        } else {
+            int from = Integer.parseInt(request.getParameter("from"));
+            int to = Integer.parseInt(request.getParameter("to"));
+
+            response.setContentType("text/csv");
+
+            try {
+                db.printTableCSV(config.getTable(), config.getFields(), from, to, out);
+            } catch (Exception e) {
+                System.out.println("ERROR: Could not acces data in DB");
+                System.out.println(e.getMessage());
+                return;
+            }
         }
 
         out.flush();
